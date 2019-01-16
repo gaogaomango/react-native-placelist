b01275734832b658f9c29336b343a767d4c2b951
Ogai Masataka <gaogaomango@gmail.com>
2019-01-16 22:34:32 +1100

    add login style

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
-import { View, Text, Button, StyleSheet, SafeAreaView } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  TextInput,
+  StyleSheet,
+  SafeAreaView
+} from "react-native";
 
 import startMainTabs from "../MainTabs/startMainTab";
+import DefaultInput from "../../components/UI/DefaultInput/DefaultInput";
 
 class AuthScreen extends Component {
   loginHandler = () => {
@@ -11,8 +19,17 @@ class AuthScreen extends Component {
   render() {
     return (
       <SafeAreaView style={styles.safeAreaContainer}>
-        <View>
-          <Text>Auth Screen</Text>
+        <View style={styles.container}>
+          <Text>Please Login</Text>
+          <Button title="Switch to Login" />
+          <View style={styles.inputContainer}>
+            <DefaultInput
+              placeholder="Your E-Mail Address"
+              style={styles.input}
+            />
+            <DefaultInput placeholder="Password" style={styles.input} />
+            <DefaultInput placeholder="Confirm Password" style={styles.input} />
+          </View>
           <Button title="Login" onPress={this.loginHandler} />
         </View>
       </SafeAreaView>
@@ -28,9 +45,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    justifyContent: "flex-start",
+    justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#F5FCFF"
+  },
+  inputContainer: {
+    width: "80%"
+  },
+  input: {
+    backgroundColor: "#eee",
+    borderColor: "#ccc"
   }
 });
 
--- src/screens/FindPlace/FindPlace.js
+++ src/screens/FindPlace/FindPlace.js
@@ -6,6 +6,23 @@ import { connect } from "react-redux";
 import PlaceList from "../../components/PlaceList/PlaceList";
 
 class FindPlaceScreen extends Component {
+  constructor(props) {
+    super(props);
+    Navigation.events().bindComponent(this);
+  }
+
+  navigationButtonPressed = event => {
+    if (event.buttonId === "sideDrawerToggle") {
+      Navigation.mergeOptions(this.props.componentId, {
+        sideMenu: {
+          left: {
+            visible: true
+          }
+        }
+      });
+    }
+  };
+
   itemSelectedHandler = key => {
     const setPlace = this.props.places.find(place => {
       return place.key == key;
